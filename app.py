4adf9a9c7bebecc7269fd4646b67d4c1028a4af8
Gabriel <gbtavares00@gmail.com>
2026-02-06 21:27:58 -0400

    study: configure Flask app with env variables, routes and Jinja templates

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def home():
-    return "Hello, World!"
+    return render_template('Index.html')
+
+
+@app.route("/nome")
+def exibir_nome():
+    return "Gabriel"
+
+@app.route("/nome/sobrenome")
+def exibir_sobrenome():
+    return "Tavares"
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv("FLASK_HOST"),
+        port=os.getenv("FLASK_PORT"),
+        debug=os.getenv("FLASK_DEBUG") == "1"
+        )
